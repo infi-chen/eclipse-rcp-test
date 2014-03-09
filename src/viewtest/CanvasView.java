1efa57821112bf1fe50fd7b61f3573391e7251af
infi-chen <infi.chen@tcl.com>
2014-03-09 09:12:46 +0800

    clear workspace.add canvas view.

---
+++ src/viewtest/CanvasView.java
@@ -0,0 +1,24 @@
+package viewtest;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class CanvasView extends ViewPart {
+
+	public CanvasView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
